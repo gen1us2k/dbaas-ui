9226b942ee0d86faf831e77443a9a7a82d3e17ed
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2023-01-18 15:37:17 +0400

    add env var

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -1,3 +1,4 @@
+const { REACT_APP_API_URL } = process.env;
 export const namespace = 'default';
-export const serverProxyUrl = `http://localhost:8080/proxy`;
-export const serverUrl = 'http://localhost:8080';
+export const serverProxyUrl = `${REACT_APP_API_URL}/proxy`;
+export const serverUrl = REACT_APP_API_URL;
